044f63344491c4b55304226e17107e17502bae9d
simon21-meet <simon21@meet.mit.edu>
2019-07-23 12:40:26 -0400

    Stopping here

--- day3/lab-part 2-Greeting.py
+++ day3/lab-part 2-Greeting.py
@@ -5,5 +5,5 @@ len(name)
 str(len(name))
 print("Your name is " + str(len(name)) + ' letters long!') 
 print("The first letter of your name is " + '"' + name[0]+ '"' + ' and the last letter is ' '"' +name[-1]+ '"')
-
+print("Missing letters: "+ str(name[1:-1]))
 
