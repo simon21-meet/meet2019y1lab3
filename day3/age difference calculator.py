78b92a01058305c272df1666846b6c2a670886ca
simon21-meet <simon21@meet.mit.edu>
2019-07-23 05:18:58 -0400

    Stopping here

---
+++ day3/age difference calculator.py
@@ -0,0 +1,6 @@
+person_one=input("What is the age of the first person?")
+person_two=input("Whatis the age of the second person?")
+person_one=int(person_one)
+person_two=int(person_two)
+print(abs(person_one-person_two))
+
