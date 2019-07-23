ea796d7828dc17a5cb974cb46792215c623e325f
simon21-meet <simon21@meet.mit.edu>
2019-07-23 07:19:24 -0400

    Stopping here

---
+++ day3/lab-part 2-Greeting.py
@@ -0,0 +1,9 @@
+name=input("What is your name?")
+print(name.capitalize())
+print("Hello there " + name.capitalize())
+len(name)
+str(len(name))
+print("Your name is " + str(len(name)) + ' letters long!') 
+print("The first letter of your name is " + '"' + name[0]+ '"' + ' and the last letter is ' '"' +name[-1]+ '"')
+
+
